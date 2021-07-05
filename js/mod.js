ebfbd25f8ede1e03c9cc01145544375697231fb2
BobH-Infiniti <86919568+BobH-Infiniti@users.noreply.github.com>
2021-07-05 20:40:53 +0700

    Yes
    Yes

--- js/mod.js
+++ js/mod.js
@@ -57,7 +57,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e694206969420"))
+	return player.points.gte(new Decimal("e69"))
 }
 
 
